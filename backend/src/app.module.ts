3ccb2dedfe0116c7803beb856de4c0b6c9d70670
Uberth Hernandez <ing.uberthhernandez@gmail.com>
2026-01-08 14:12:33 -0500

    feat: configured typeorm with the database

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app/app.controller';
 import configuration from './config/configuration';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { databaseConfig } from './config/database.config';
 
 @Module({
-  imports: [ConfigModule.forRoot(configuration)],
+  imports: [ConfigModule.forRoot(configuration),
+    TypeOrmModule.forRootAsync({
+      imports:  [ConfigModule],
+      useFactory: databaseConfig,
+      inject: [ConfigService]
+    })
+  ],
   controllers: [AppController],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
