6ef43e1a08306576da59e8601350f3e394caa2a9
Uberth Hernandez <ing.uberthhernandez@gmail.com>
2026-01-21 13:17:17 -0500

    Feat: remove typeorm and add Prisma orm

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,21 +1,11 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app/app.controller';
 import configuration from './config/configuration';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { databaseConfig } from './config/database.config';
 import { LoggerModule } from './common/logger/logger.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(configuration),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: databaseConfig,
-      inject: [ConfigService],
-    }),
-    LoggerModule,
-  ],
+  imports: [ConfigModule.forRoot(configuration), LoggerModule],
   controllers: [AppController],
   providers: [],
 })
