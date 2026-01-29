ee5e07375f69f924d560a8069a7849977b7389b9
Uberth Hernandez <ing.uberthhernandez@gmail.com>
2026-01-29 10:46:42 -0500

    Feat: add global validation pipe

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['log', 'error', 'warn', 'debug', 'verbose'],
   });
   app.setGlobalPrefix('api/v1');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
 
   const PORT = app.get(ConfigService).get('PORT');
   const ENV = app.get(ConfigService).get('NODE_ENV');
