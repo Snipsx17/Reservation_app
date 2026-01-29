8f602bda1d05ed14b8bb218f9af3eca7ba94193c
Uberth Hernandez <ing.uberthhernandez@gmail.com>
2026-01-29 10:39:52 -0500

    Feat: add methods to has and compare passwords

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,18 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from 'src/users/user.service';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
   constructor(
     private usersService: UsersService,
     private jwtService: JwtService,
+    private configService: ConfigService,
   ) {}
 
-  async validate(email: string, pass: string) {
-    const user = await this.usersService.findOne(email);
-    console.log(user.password, pass);
-    if (user && user.password === pass) {
+  async validate(username: string, pass: string) {
+    const user = await this.usersService.findOne(username);
+
+    if (user && (await this.comparePasswords(pass, user.password))) {
       const { password, ...result } = user;
       return result;
     }
@@ -20,10 +24,35 @@ export class AuthService {
     return null;
   }
 
-  async login(user: any) {
-    const payload = { email: user.email, sub: user.id };
+  async login(username: string, pass: string) {
+    const user = await this.usersService.findOne(username);
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    if (await this.comparePasswords(pass, user.password)) {
+      throw new UnauthorizedException();
+    }
+
+    const payload = { username: user.username, sub: user.id };
+
     return {
       access_token: this.jwtService.sign(payload),
     };
+  }
+
+  async hashPassword(password: string): Promise<string> {
+    return await bcrypt.hash(
+      password,
+      this.configService.get<number>('JWT_SALT_ROUNDS'),
+    );
+  }
+
+  async comparePasswords(
+    password: string,
+    hashedPassword: string,
+  ): Promise<boolean> {
+    return await bcrypt.compare(password, hashedPassword);
   }
 }
