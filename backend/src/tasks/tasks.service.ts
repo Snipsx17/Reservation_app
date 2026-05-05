02a2afc2ad8efcf52aa83e10b7190b738dd1a854
Uberth Hernandez <ing.uberthhernandez@gmail.com>
2026-05-05 11:53:20 -0400

    feat: added error handler

--- backend/src/tasks/tasks.service.ts
+++ backend/src/tasks/tasks.service.ts
@@ -1,22 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { PrismaService } from '@/prisma/prisma.service';
+import { ErrorHandler } from '@/common/helpers/error-handler.helper';
+import { LoggerService } from '@/common/logger/logger.service';
 
 @Injectable()
 export class TasksService {
-  constructor(private readonly dbService: PrismaService) {}
+  constructor(
+    private readonly dbService: PrismaService,
+    private readonly logger: LoggerService,
+  ) {}
 
   @Cron(CronExpression.EVERY_DAY_AT_2AM, {
     name: 'Clean expired refresh tokens',
     timeZone: 'Europe/Madrid',
   })
   async handleCron() {
-    await this.dbService.refreshTokens.deleteMany({
-      where: {
-        expiresAt: {
-          lte: new Date(),
+    try {
+      await this.dbService.refreshTokens.deleteMany({
+        where: {
+          expiresAt: {
+            lte: new Date(),
+          },
         },
-      },
-    });
+      });
+    } catch (error) {
+      ErrorHandler.handle(
+        error,
+        'TaskService.handleCron',
+        `Error creating cron task`,
+        this.logger,
+      );
+    }
   }
 }
--- backend/src/users/user.service.ts
+++ backend/src/users/user.service.ts
@@ -1,21 +1,21 @@
-import {
-  ConflictException,
-  Injectable,
-  InternalServerErrorException,
-} from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { PrismaService } from '@/prisma/prisma.service';
 import { User, UserRole } from '../generated/prisma/client';
 import { CreateUserDto } from './dto/create-user.dto';
 import uuidGenerator from '@/common/helpers/uuid-generator';
 import { CryptoHelper } from '@/common/helpers/crypto.helper';
 import { ConfigService } from '@nestjs/config';
-import { PrismaClientKnownRequestError } from '@prisma/client/runtime/client';
+import { ErrorHandler } from '@/common/helpers/error-handler.helper';
+import { LoggerService } from '@/common/logger/logger.service';
+import { UserResponseDto } from '@/auth/dto/user-response.dto';
+import { plainToClass } from 'class-transformer';
 
 @Injectable()
 export class UsersService {
   constructor(
     private readonly dbService: PrismaService,
     private readonly configService: ConfigService,
+    private readonly logger: LoggerService,
   ) {}
 
   async findOne(username: string): Promise<User | null> {
@@ -25,16 +25,19 @@ export class UsersService {
       });
       return user ? user : null;
     } catch (error) {
-      if (error instanceof PrismaClientKnownRequestError)
-        throw new InternalServerErrorException('Error during login');
-      throw new InternalServerErrorException();
+      ErrorHandler.handle(
+        error,
+        'userService.findOne',
+        `Error during login`,
+        this.logger,
+      );
     }
   }
 
   async create(
     user: CreateUserDto,
     role: UserRole = UserRole.ADMIN,
-  ): Promise<User> {
+  ): Promise<UserResponseDto> {
     try {
       const userExist = await this.findOne(user.username);
 
@@ -53,14 +56,22 @@ export class UsersService {
         role,
       };
 
-      return await this.dbService.user.create({
+      const newDbUser = await this.dbService.user.create({
         data: newUser,
       });
-    } catch (error) {
-      if (error instanceof PrismaClientKnownRequestError)
-        throw new InternalServerErrorException('Error during login');
 
-      throw new InternalServerErrorException();
+      if (!user) return null;
+
+      return plainToClass(UserResponseDto, newDbUser, {
+        excludeExtraneousValues: true,
+      });
+    } catch (error) {
+      ErrorHandler.handle(
+        error,
+        'userService.create',
+        `Error creating new User`,
+        this.logger,
+      );
     }
   }
 }
