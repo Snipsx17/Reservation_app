7a018bf6f649e85d4215c989b789145f8778e2b3
Uberth Hernandez <ing.uberthhernandez@gmail.com>
2026-06-15 15:48:09 -0400

    feat: update db schema

--- backend/prisma/migrations/20260507155712_update_refresh_token_size/migration.sql
+++ backend/prisma/migrations/20260507155712_update_refresh_token_size/migration.sql
@@ -1,2 +1,5 @@
 -- AlterTable
 ALTER TABLE "refresh_tokens" ALTER COLUMN "token" SET DATA TYPE VARCHAR(256);
+
+-- AlterTable
+ALTER TABLE "users" ALTER COLUMN "refreshToken" SET DATA TYPE VARCHAR(512);
