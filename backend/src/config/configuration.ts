86369795b4a8378de1f16c6209cc898654105921
Uberth Hernandez <ing.uberthhernandez@gmail.com>
2026-06-16 11:55:59 -0400

    fix: JWT_REFRESH_TOKEN_EXPIRATION type

--- backend/src/config/configuration.ts
+++ backend/src/config/configuration.ts
@@ -18,7 +18,7 @@ export default {
     JWT_ACCESS_TOKEN_SECRET: Joi.string().required(),
     JWT_ACCESS_TOKEN_EXPIRATION: Joi.number().required(),
     JWT_REFRESH_TOKEN_SECRET: Joi.string().required(),
-    JWT_REFRESH_TOKEN_EXPIRATION: Joi.string().required(),
+    JWT_REFRESH_TOKEN_EXPIRATION: Joi.number().required(),
     JWT_SALT_ROUNDS: Joi.number().required(),
   }),
   isGlobal: true,
